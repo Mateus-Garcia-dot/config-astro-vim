e2a5369cd67f5ad36259f468d8af45aeb0d8c1a4
Mateus Medeiros Garcia <mateusm.garcia0@gmail.com>
2023-03-18 19:48:18 -0300

    added my own config of norg

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,5 @@
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.note-taking.neorg" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
