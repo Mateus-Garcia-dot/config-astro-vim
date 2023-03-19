a627f5f5ccc7e8595ab23c2882ea9fb173e78e4f
Mateus Medeiros Garcia <mateusm.garcia0@gmail.com>
2023-03-19 16:54:13 -0300

    added cmake plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,4 +2,5 @@ return {
   "AstroNvim/astrocommunity",
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.cmake" },
 }
