7bfa431e5d00a4a9ff120c482d13dd038b126788
Mateus Medeiros Garcia <mateusm.garcia0@gmail.com>
2023-03-24 22:01:35 -0300

    disabled mouse

--- options.lua
+++ options.lua
@@ -7,7 +7,8 @@ return {
     spell = false,         -- sets vim.opt.spell
     signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
     wrap = false,          -- sets vim.opt.wrap
-    conceallevel = 3
+    conceallevel = 3,
+    mouse = 'c',
   },
   g = {
     mapleader = " ",                 -- sets vim.g.mapleader
