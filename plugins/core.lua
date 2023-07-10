7d5d3c444d6e03cb2f3d611718ef01fd6ce3e572
mateus <mateusm.garcia0@gmail.com>
2023-07-10 10:33:54 -0300

    add ascii art

--- plugins/core.lua
+++ plugins/core.lua
@@ -4,20 +4,46 @@ return {
     "goolord/alpha-nvim",
     opts = function(_, opts)
       -- customize the dashboard header
-      opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
+      require("alpha")
+      require("alpha.term")
+      local dashboard = require("alpha.themes.dashboard")
+      dashboard.section.buttons.val = {
+        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+        dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+        dashboard.button("s", "勒" .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
       }
-      return opts
+      for _, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButtons"
+        button.opts.hl_shortcut = "AlphaShortcut"
+      end
+      dashboard.section.footer.opts.hl = "Type"
+      dashboard.section.header.opts.hl = "AlphaShortcut"
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+
+      local width = 46
+      local height = 25 -- two pixels per vertical space
+      dashboard.section.terminal.command = "cat | " ..
+          os.getenv("HOME") .. "/.config/nvim/lua/user/scripts/thisisfine.sh"
+      dashboard.section.terminal.width = width
+      dashboard.section.terminal.height = height
+      dashboard.section.terminal.opts.redraw = true
+
+      dashboard.section.header.val = ""
+
+      dashboard.config.layout = {
+        dashboard.section.terminal,
+        { type = "padding", val = 5 },
+        dashboard.section.header,
+        { type = "padding", val = 1 },
+        dashboard.section.buttons,
+        { type = "padding", val = 1 },
+        dashboard.section.footer,
+      }
+
+      return dashboard
     end,
   },
   -- You can disable default plugins as follows:
